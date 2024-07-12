93ffc3780a023fcf1e64f975ed310b426a9d5d71
= <=>
2024-07-12 19:25:21 +0300

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+
 public class Main {
     public static void main(String[] args) {
+        int[] u = {1, 0, 4, 5};
+        int[] v = {0, 4, -5, 4};
+        isOrtogonal(u, v);
 
+    }
+
+    static void isOrtogonal(int[] u, int[] v) {
+        if (u.length != v.length) {
+            System.out.println("Boyutları farklı.Ortogonal Değil");
+        } else {//boyutlar aynıysa
+            if(dot(u,v)==0){
+                System.out.println("Ortogonaldir");
+            }else{
+                System.out.println("Ortogonal Değildir");
+            }
+
+        }
+    }
+    static int dot(int[] u, int[] v) {
+        int sum = 0;
+        for (int i = 0; i < u.length; i++) {
+            sum += u[i] * v[i];
+        }
+        return sum;
     }
 }
